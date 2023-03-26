5ecc6e31e6be6b42e6bd3cf488c02b6c1c8686f1
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-26 11:03:45 +0100

    refactored TechPodApplication to Main

--- src/main/java/org/tech_pod/Main.java
+++ src/main/java/org/tech_pod/Main.java
@@ -1,13 +1,8 @@
 package org.tech_pod;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-    public class TechPodApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TechPodApplication.class, args);
-    }
+    public class Main {
+    public static void main(String[] args) { SpringApplication.run(Main.class, args);}
 }
