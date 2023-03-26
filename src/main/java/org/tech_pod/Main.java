1ad2e306b699181121704f34f04bf256bfbb52b8
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-26 10:53:25 +0100

    building out backend files

---
+++ src/main/java/org/tech_pod/Main.java
@@ -0,0 +1,13 @@
+package org.tech_pod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+    public class TechPodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechPodApplication.class, args);
+    }
+}
---
+++ src/main/java/org/tech_pod/components/Dataloader.java
@@ -0,0 +1,10 @@
+package org.tech_pod.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dataloader implements ApplicationRunner {
+
