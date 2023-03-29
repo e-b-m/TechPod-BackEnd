013edf4f2a82959d2cf451c84716ef5896c6c863
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-29 21:13:48 +0100

    renamed file

--- src/main/java/org/tech_pod/model/User.java
+++ src/main/java/org/tech_pod/model/User.java
@@ -3,7 +3,7 @@ package org.tech_pod.model;
 
 import javax.persistence.*;
 
-@Entity(name="users")
+@Entity(name="user_profiles")
 public class User {
 
     @Id
@@ -12,6 +12,24 @@ public class User {
 
     @Column(name = "name")
     private String name;
+
+    @Column (name = "age")
+    private int age;
+
+    @Column (name = "email")
+    private char[] email;
+
+    @Column (name = "password")
+    private char[] password;
+
+    @Column (name = "bio")
+    private String bio;
+
+    @Column (name = "occupation")
+    private String occupation;
+
+
+
 
 
 
