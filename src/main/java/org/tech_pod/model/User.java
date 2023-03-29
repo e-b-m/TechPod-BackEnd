b617519c6044bb985784b5d3a30a5f6d9b6df639
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-29 22:06:04 +0100

    added getters and setters to User

--- src/main/java/org/tech_pod/model/User.java
+++ src/main/java/org/tech_pod/model/User.java
@@ -1,8 +1,10 @@
 package org.tech_pod.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 
 import javax.persistence.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name="user")
@@ -56,5 +58,75 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Resource> resources;
+
+    public User(String name, int age, char[] email, char[] password, String bio, String occupation, URL websites){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+        this.occupation = occupation;
+        this.websites = websites;
+        this.events = new ArrayList<Event>();
+        this.jobPosts = new ArrayList<JobPost>();
+        this.blogPosts = new ArrayList<BlogPost>();
+        this.resources = new ArrayList<Resource>();
+    }
+
+    public User(){}
+
+    // Getters & Setters
+
+    public Long getId() {return id};
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public int getAge() {return age;}
+
+    public void setAge(int age) {this.age = age;}
+
+    public char[] getEmail() {return email;}
+
+    public void setEmail(char[] email) {this.email = email;}
+
+    public char[] getPassword() {return password;}
+
+    public void setPassword(char[] password) {this.password = password;}
+
+    public String getBio(){return bio;}
+
+    public void setBio(String bio){this.bio = bio;}
+
+    public String getOccupation(){return occupation;}
+
+    public void setOccupation(String occupation) {this.occupation = occupation;}
+
+    public URL getWebsites() {return websites;}
+
+    public void setWebsites(URL websites){this.websites = websites;}
+
+    public List<Event> getEvents() {return events;}
+
+    public void setEvents(List<Event> events) {this.events = events;}
+
+    public List<JobPost> getJobPosts() {return jobPosts;}
+
+    public void setJobPosts(List<JobPost> jobPosts){this.jobPosts = jobPosts;}
+
+    public List<BlogPost> getBlogPosts() {return blogPosts;}
+
+    public void setBlogPosts(List<BlogPost> blogPosts){this.blogPosts = blogPosts;}
+
+    public List<Resource> getResources(){return resources;}
+
+    public void setResources(List<Resource> resources){this.resources = resources;}
+
+
+
+
 }
 
