cdd99deae99bd50cc461fbc3c325c53f6a3bb45f
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-30 07:19:17 +0100

    added data for JobPost

--- src/main/java/org/tech_pod/model/JobPost.java
+++ src/main/java/org/tech_pod/model/JobPost.java
@@ -1,6 +1,51 @@
 package org.tech_pod.model;
-import javax.persistence.Entity;
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity(name = "job_posts")
 public class JobPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "deadline")
+    private LocalDate deadline;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public JobPost (String title, LocalDate startDate, LocalDate deadline, String location, int salary, String description, User user){
+        this.title = title;
+        this.startDate = startDate;
+        this.deadline = deadline;
+        this.location = location;
+        this.salary = salary;
+        this.description = description;
+        this.user = user;
+    }
+
+    public JobPost(){}
+
+    // Getters & Setters
+
+
 }
